6ecc98adae94f4cb8e9e810558104e9d8fe094bc
c-young02 <callum.young02@hotmail.com>
2024-02-10 15:34:48 +0000

    Fixed error with uploading images

--- website/src/components/Buttons/UploadButton/UploadButtonContainer.js
+++ website/src/components/Buttons/UploadButton/UploadButtonContainer.js
@@ -1,54 +1,47 @@
 import React from 'react';
 import UploadButton from './UploadButton';
 
-// UploadButtonContainer is a functional component that receives props and returns JSX.
-// It handles the logic for uploading an image, including sanitizing the file name and checking the file type and size.
+// This component handles the image upload functionality.
 function UploadButtonContainer({ onImageSelect }) {
-	// sanitizeFileName is a helper function that sanitizes the file name by replacing any character
-	// that is not a letter, number, hyphen, underscore, or period with an underscore.
-	const sanitizeFileName = (name) => {
-		return name.replace(/[^a-z0-9\-_.]/gi, '_');
-	};
-
-	// handleImageUpload is a function that is called when a file is selected for upload.
+	// This function is triggered when a file is selected for upload.
 	const handleImageUpload = (event) => {
-		// Get the selected file.
+		// Get the first file from the list of selected files.
 		const file = event.target.files[0];
 
-		// Check if a file was selected.
+		// If no file was selected, show an alert and stop execution.
 		if (!file) {
 			alert('No file selected for upload.');
 			return;
 		}
 
-		// Sanitize the file name.
-		file.name = sanitizeFileName(file.name);
-
-		// Check the file type.
+		// If the selected file is not an image, show an alert and stop execution.
 		if (!file.type.startsWith('image/')) {
 			alert('Selected file is not an image.');
 			return;
 		}
 
-		// Check the file size (max 5MB).
+		// Define the maximum file size (5MB in this case).
 		const maxSize = 5 * 1024 * 1024; // 5MB
+
+		// If the selected file is larger than the maximum size, show an alert and stop execution.
 		if (file.size > maxSize) {
 			alert('Selected file is too large.');
 			return;
 		}
 
-		// Create a new FileReader object.
+		// Create a new FileReader object to read the contents of the file.
 		const reader = new FileReader();
 
-		// This event listener is called when the file read operation is complete.
+		// This event listener is triggered when the file read operation is complete.
 		reader.onloadend = () => {
 			// Get the base64 string of the uploaded image.
 			let base64Result = reader.result.split(',')[1];
+
 			// Call the onImageSelect function passed as a prop with the base64 string.
 			onImageSelect(base64Result);
 		};
 
-		// Read the file as a data URL.
+		// Start reading the file as a data URL.
 		reader.readAsDataURL(file);
 	};
 
@@ -56,4 +49,5 @@ function UploadButtonContainer({ onImageSelect }) {
 	return <UploadButton onUpload={handleImageUpload} />;
 }
 
+// Export the UploadButtonContainer component.
 export default UploadButtonContainer;
